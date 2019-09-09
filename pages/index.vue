6996e1524fdc2312dfcc70f96dc4527bd34ec2fb
Hartmann, Eric <coguy450@gmail.com>
2019-09-09 14:08:13 -0600

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6 >
+      <div class="text-center">
+        Eric's content here
+      </div>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to Your butt
+        </v-card-title>
+        <v-card-text>
+          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
+        
+          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
+          <div class="text-xs-right">
+            <em><small>&mdash; John Leider</small></em>
+          </div>
+          <hr class="my-3">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+          >
+            Nuxt Documentation
+          </a>
+          <br>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+          >
+            Nuxt GitHub
+          </a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
---
+++ pages/servers.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-layout column justify-center align-center>
+    <p>Servers and stuff here</p>
+  </v-layout >
+</template>
